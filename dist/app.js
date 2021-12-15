3419140d3aa326f8db2d3477d5ae33656dd9f6ff
Eduardo Olea <laloolea18@gmail.com>
2021-12-15 14:38:52 -0700

    finished

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log("Hola");
+var STATUS;
+(function (STATUS) {
+    STATUS[STATUS["ACTIVO"] = 0] = "ACTIVO";
+    STATUS[STATUS["BAJA"] = 1] = "BAJA";
+})(STATUS || (STATUS = {}));
+class Persona {
+    constructor(nombre, apellido, nac, ciudad, pais) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nac = nac;
+        this.ciudad = ciudad;
+        this.pais = pais;
+        this._edad = new Date().getFullYear() - this.nac.getFullYear();
+    }
+    get edad() {
+        return this._edad;
+    }
+}
+class Estudiante extends Persona {
+    constructor(nombre, apellido, nac, ciudad, pais, carrera, campus, status, fechaIngreso) {
+        super(nombre, apellido, nac, ciudad, pais);
+        this.carrera = carrera;
+        this.campus = campus;
+        this.status = status;
+        this.fechaIngreso = fechaIngreso;
+    }
+    getStudentData() {
+        return `Nombre del alumno: ${this.nombre} ${this.apellido}, edad: ${this.edad}, campus: ${this.campus}`;
+    }
+}
+const lalo = new Estudiante("Eduardo", "Gonzalez", new Date(1997, 12, 18), "Hermosillo", "Mexico", "Ciencias de la Computacion", "Unison", STATUS.ACTIVO, new Date(2016, 7, 10));
+console.log(lalo);
+console.log(lalo.edad);
+console.log(lalo.getStudentData());
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,50 @@
-console.log("Hola"); 
+enum STATUS{ACTIVO,BAJA}
+//Creacion de la clase abstracta Persona
+// interface Person{
+//     nombre:string;
+//     edad:number;
+//     nac:Date;
+//     ciudad:string;
+//     pais:string;
+// }
+
+abstract class Persona/* implements Person*/{
+
+    protected _edad:number;
+    constructor(protected nombre:string,protected apellido:string, protected nac:Date, protected ciudad:string,protected pais:string){
+        //Se calcula la edad en base al año actual menos el año que nacio.
+
+
+        this._edad= new Date().getFullYear() - this.nac.getFullYear(); 
+    }
+       //Metodo para devover la edad
+    public get edad(){
+        return this._edad;
+    } 
+  
+
+}
+
+//Creacion de la clase 
+class Estudiante extends Persona{
+    constructor(  nombre:string,  apellido:string,  nac:Date,  ciudad:string,   pais:string, protected carrera:string,protected campus:string,protected status:number,protected fechaIngreso:Date){
+        super(nombre,apellido,nac,ciudad,pais);
+        
+    }
+    //Metodo que devuelve datos de el estudiante
+    getStudentData():string{
+
+        return `Nombre del alumno: ${this.nombre} ${this.apellido}, edad: ${this.edad}, campus: ${this.campus}`;
+    }
+
+}
+
+//Creamos un alumno para probar la clase
+const lalo = new Estudiante("Eduardo","Gonzalez",new Date(1997, 12, 18),"Hermosillo","Mexico","Ciencias de la Computacion","Unison",STATUS.ACTIVO,new Date(2016, 7, 10));
+
+//Se imprimem sus datos
+console.log(lalo);
+//se muestra su edad utilizando el .get
+console.log(lalo.edad); 
+//Mostramos sus datos
+console.log(lalo.getStudentData());
